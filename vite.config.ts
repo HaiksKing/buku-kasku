9870664cf193cd8795faa961ff329031b693fede
HaiksKing <fengkir@gmail.com>
2025-12-26 11:18:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Baris di bawah ini adalah perbaikannya:
-  base: '/buku-kasku/', 
+  base: './', 
   server: {
     host: "::",
     port: 8080,
