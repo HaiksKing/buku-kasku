baf53f16437a830a2a9512254ab8f8ad31adeac1
HaiksKing <fengkir@gmail.com>
2025-12-26 18:38:36 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Mengubah BrowserRouter menjadi HashRouter
 import Index from "./pages/Index";
 import BuatBukuKas from "./pages/BuatBukuKas";
 import DaftarBukuKas from "./pages/DaftarBukuKas";
@@ -16,17 +16,17 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* Tambahkan basename di bawah ini */}
-      <BrowserRouter basename="/buku-kasku">
+      {/* HashRouter tidak memerlukan 'basename', sehingga lebih aman dari error refresh */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/buat-buku-kas" element={<BuatBukuKas />} />
           <Route path="/daftar-buku-kas" element={<DaftarBukuKas />} />
           <Route path="/dashboard/:id" element={<Dashboard />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* SEMUA RUTE CUSTOM DI ATAS RUTE "*" */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
